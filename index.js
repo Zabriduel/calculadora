889042c909e123da641207cdbf51b4cab8c90e01
mariabigati <maria57souza78@gmail.com>
2025-03-19 11:29:34 -0300

    Incluído funcionalidade de adição.

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ let numeroUm = parseFloat(prompt("Digite o primeiro número para calcular: "));
 let numeroDois = parseFloat(prompt("Digite o segundo número para calcular: "));
 let selecionarOperacao = prompt("Digite qual operação desejada +, -, *, /: ");
 
+if (selecionarOperacao == '+') {
+    let soma = primeiroNum + segundoNum;
+    console.log(`Resultado: ${soma}`);
+};
+
+
