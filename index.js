2211b1ff8c26cbd47789ebcd22e1eda8600f18af
Zabriduel <gabriel31alves@gmail.com>
2025-03-19 11:14:54 -0300

    Adicionado divisão

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ let numeroUm = parseFloat(prompt("Digite o primeiro número para calcular: "));
 let numeroDois = parseFloat(prompt("Digite o segundo número para calcular: "));
 let selecionarOperacao = prompt("Digite qual operação desejada +, -, *, /: ");
 
+
+if (selecionarOperacao == "/"){
+    let resultado = numeroUm / numeroDois;
+    console.log(`A divisão de ${numeroUm} / ${numeroDois} = ${resultado} `);
+}
+else{
+    console.log("Opção inválida")
+}
