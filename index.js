1637089ec6627b3a88d2ea65652662ae585d7e44
Zabriduel <gabriel31alves@gmail.com>
2025-03-19 10:51:42 -0300

    Criando menu

---
+++ index.js
@@ -0,0 +1,4 @@
+let numeroUm = parseFloat(prompt("Digite o primeiro número para calcular: "));
+let numeroDois = parseFloat(prompt("Digite o segundo número para calcular: "));
+let selecionarOperacao = prompt("Digite qual operação desejada +, -, *, /: ");
+
